23cfdef9071127a827db33b7f7a7d1a31b5710ea
tenso2006 <tenso2006@gmail.com>
2017-01-06 15:14:00 -0800

    done till GroceryList to dynamically render an array

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+var Item1 = () => (
+  <li>milk</li>
+);
+
+var Item2 = () => (
+  <li>water</li>
+);
+
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => 
+      (<GroceryListItem item={item} />)
+    )}
+  </ul>
+);
+
+
+
+
+ReactDOM.render(<GroceryList items={['milk', 'water', 'salt','vegetable','sugar', 'pepper']}/>, document.getElementById('app'));
